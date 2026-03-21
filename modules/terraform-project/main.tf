af4c8cd99ff615bbe5d2c342a4b09eff92f58cdf
matustoth <55988328+karafra@users.noreply.github.com>
2026-03-21 19:37:29 +0100

    Fix execution moe

--- modules/terraform-project/main.tf
+++ modules/terraform-project/main.tf
@@ -6,6 +6,6 @@ data "tfe_organization" "organization" {
 resource "tfe_project" "project" {
   for_each     = { for project in var.projects : project.name => project }
   name         = each.key
-  organization = data.tfe_organization.organization.id
+  organization = data.tfe_organization.organization[each.key].id
   tags         = merge(each.value.additional_tags, local.default_project_tags)
 }
