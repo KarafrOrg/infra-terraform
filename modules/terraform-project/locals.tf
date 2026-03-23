44ead3399c2af8d8de9ce18905713fbb40f948ef
matustoth <55988328+karafra@users.noreply.github.com>
2026-03-23 22:41:23 +0100

    Align naming

--- modules/terraform-project/locals.tf
+++ modules/terraform-project/locals.tf
@@ -2,5 +2,5 @@ locals {
   default_project_tags = {
     "managed_by" = "terraform"
   }
-  default_variable_set_name = "default-project-variable-set"
+  default_variable_set_name = "default_project_variable_set"
 }
