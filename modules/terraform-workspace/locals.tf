e0dcaf6443579b6fe82536e61f6f4d7102f7cf47
matustoth <55988328+karafra@users.noreply.github.com>
2026-03-23 22:40:14 +0100

    Align naming

--- modules/terraform-workspace/locals.tf
+++ modules/terraform-workspace/locals.tf
@@ -2,5 +2,5 @@ locals {
   default_workspace_tags = {
     "managed_by" = "terraform"
   }
-  default_variable_set_name_prefix = "default-workspace-variable-set"
+  default_variable_set_name_prefix = "default_workspace_variable_set"
 }
