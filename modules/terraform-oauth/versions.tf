97496f67db4ccd6fe365084ad33cba0b827a1bab
matustoth <55988328+karafra@users.noreply.github.com>
2026-03-24 21:44:27 +0100

    Removed unused provider

--- modules/terraform-oauth/versions.tf
+++ modules/terraform-oauth/versions.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "~> 6.6"
-    }
     tfe = {
       source  = "hashicorp/tfe"
       version = "~> 0.70"
