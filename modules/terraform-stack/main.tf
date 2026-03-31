7d0e1dc8c953989624f7114bfa357396bbe14f68
matustoth <55988328+karafra@users.noreply.github.com>
2026-03-31 22:58:11 +0200

    Update lock

--- modules/terraform-stack/main.tf
+++ modules/terraform-stack/main.tf
@@ -9,24 +9,10 @@ data "tfe_project" "parent" {
   organization = each.value.organization_name
 }
 
-resource "null_resource" "debug" {
-    for_each = { for stack in var.stacks : stack.name => stack }
-    provisioner "local-exec" {
-        command = <<EOT
-        echo "Stack Name: ${each.value.name}"
-        echo "Project Name: ${each.value.project_name}"
-        echo "Organization Name: ${each.value.organization_name}"
-        echo "Repository Name: ${data.github_repository.repository[each.key].name}"
-        echo "Project ID: ${data.tfe_project.parent[each.key].id}"
-        EOT
-    }
-}
-
 resource "tfe_stack" "managed" {
   for_each   = { for stack in var.stacks : stack.name => stack }
   name       = each.value.name
-#  project_id = data.tfe_project.parent[each.key].id
-  project_id = "prj-VhQT7fVtBs225enP"
+  project_id = data.tfe_project.parent[each.key].id
 #  vcs_repo {
 #    identifier     = "KarafrOrg/infra-terraform"
 #    branch         = each.value.repository.default_branch
