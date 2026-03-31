3604cd5a309de3ceb2e854b49d791d2407e863d2
matustoth <55988328+karafra@users.noreply.github.com>
2026-03-31 22:44:38 +0200

    Fix naming

--- modules/terraform-stack/main.tf
+++ modules/terraform-stack/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.4"
+    }
+  }
+}
 data "github_repository" "repository" {
   for_each = { for stack in var.stacks : stack.name => stack.repository }
   name     = each.value["name"]
@@ -9,10 +17,23 @@ data "tfe_project" "parent" {
   organization = each.value.organization_name
 }
 
+resource "null_resource" "debug" {
+    for_each = { for stack in var.stacks : stack.name => stack }
+    provisioner "local-exec" {
+        command = <<EOT
+        echo "Stack Name: ${each.value.name}"
+        echo "Project Name: ${each.value.project_name}"
+        echo "Organization Name: ${each.value.organization_name}"
+        echo "Repository Name: ${data.github_repository.repository[each.key].name}"
+        echo "Project ID: ${data.tfe_project.parent[each.key].id}"
+        EOT
+    }
+}
+
 resource "tfe_stack" "managed" {
   for_each   = { for stack in var.stacks : stack.name => stack }
   name       = each.value.name
-  #project_id = data.tfe_project.parent[each.key].id
+#  project_id = data.tfe_project.parent[each.key].id
   project_id = "prj-VhQT7fVtBs225enP"
 #  vcs_repo {
 #    identifier     = "KarafrOrg/infra-terraform"
